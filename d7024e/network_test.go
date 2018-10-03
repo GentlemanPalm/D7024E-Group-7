f8b93f744af1c72489242521a6524cbf5b7e3980
Palm <paljon-4@student.ltu.se>
2018-10-03 21:54:38 +0200

    Made test for FindValue RPC

--- d7024e/network_test.go
+++ d7024e/network_test.go
@@ -3,13 +3,14 @@ package d7024e
 import (
 	"NetworkMessage"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
+	"os"
 	"strconv"
 	"testing"
 	"time"
-	"io/ioutil"
-	"log"
-	"os"
+
 	"github.com/golang/protobuf/proto"
 )
 
@@ -139,6 +140,94 @@ func TestFindNode(t *testing.T) {
 
 }
 
+var findValueContent = []byte{1, 3, 3, 7}
+
+type mockFileHandler struct {
+	kvt map[string][]byte
+}
+
+func (mfh *mockFileHandler) ReadFile(hash string) []byte {
+	return mfh.kvt[hash]
+}
+
+func (mfh *mockFileHandler) WriteFile(hash string, content []byte) bool {
+	mfh.kvt[hash] = content
+	return true
+}
+
+func TestHandleFindValue(t *testing.T) {
+	network := mockNetwork()
+	mfh := &mockFileHandler{}
+	mfh.kvt = make(map[string][]byte)
+	network.storeTable.fh = mfh
+
+	mdw := &MockDataWriter{}
+	sentPacket := &PacketContainer{}
+	mdw.callback = sentPacket.findNodeCallback
+
+	hash := NewRandomKademliaID()
+
+	network.storeTable.Push(findValueContent, hash.String(), false, true)
+
+	network.dw = mdw
+
+	for i := 0; i < 25; i++ {
+		contact := NewContact(NewRandomKademliaID(), "127.0.0."+strconv.Itoa(i))
+		network.routingTable.AddContact(contact)
+	}
+
+	rid := NewRandomKademliaID()
+
+	find := &NetworkMessage.Find{}
+	find.RandomId = rid.String()
+	find.Hash = hash.String()
+
+	network.HandleFindValueMessage(find, "127.0.0.255")
+
+	if mdw.nrofTimesCalled != 1 {
+		t.Error("(HandleFindValue) Expected to have written a response to a findvalue message.")
+	}
+
+	if sentPacket.packet.Value == nil {
+		t.Error("(HandleFindValue) No nodes were provided in response to findvalue.")
+	} else {
+		switch response := sentPacket.packet.Value.Response.(type) {
+		case *NetworkMessage.ValueResponse_Nodes:
+			t.Error("(HandleFindValue) Provided key known to be in store table. Exptected data response.")
+		case *NetworkMessage.ValueResponse_Content:
+			if response.Content == nil {
+				t.Error("(HandleFindValue) Content was not the byte array as exptected")
+			} else {
+				fmt.Println("[OK] HandleFindValue handles requests known to contain data correctly.")
+			}
+		}
+	}
+
+	find = &NetworkMessage.Find{}
+	rid = NewRandomKademliaID()
+	hash = NewRandomKademliaID()
+
+	find.RandomId = rid.String()
+	find.Hash = hash.String()
+
+	network.HandleFindValueMessage(find, "127.0.0.255")
+
+	if sentPacket.packet.Value == nil {
+		t.Error("(HandleFindValue) No nodes were provided in response to findvalue.")
+	} else {
+		switch response := sentPacket.packet.Value.Response.(type) {
+		case *NetworkMessage.ValueResponse_Nodes:
+			if len(response.Nodes.Nodes) != 20 {
+				t.Errorf("(HandleFindValue) Response contains %d nodes, expected 20", len(response.Nodes.Nodes))
+			} else {
+				fmt.Println("HandleFindValue seems to work properly")
+			}
+		case *NetworkMessage.ValueResponse_Content:
+			t.Error("There should not be a value returned for a random hash. There is an infinitesimal chance this is due to a collision.")
+		}
+	}
+}
+
 func TestHandleFindNode(t *testing.T) {
 	network := mockNetwork()
 	mdw := &MockDataWriter{}
@@ -249,6 +338,7 @@ func (pc *PacketContainer) findNodeCallback(packet *NetworkMessage.Packet) {
 func TestFindValue(t *testing.T) {
 	// TODO: After FIND_VALUE is implemented and Test_FIND_NODE works
 }
+
 //Store RPC
 func TestStore(t *testing.T) {
 	network := mockNetwork()
@@ -259,10 +349,10 @@ func TestStore(t *testing.T) {
 	hash := Hash(filename)
 
 	os.Mkdir("Files", 0644)
-	filePath := "Files/" + hash 
+	filePath := "Files/" + hash
 	content, err1 := ioutil.ReadFile(filename)
-		if err1 != nil {
-			log.Fatal(err1)
+	if err1 != nil {
+		log.Fatal(err1)
 	}
 
 	err2 := ioutil.WriteFile(filePath, content, 0644)
@@ -287,6 +377,7 @@ func TestStore(t *testing.T) {
 		fmt.Println("Stor messages results in storeResponse")
 	}
 }
+
 /*func TestHandleStore(t *testing.T) {
 	network := mockNetwork()
 	mdw := &MockDataWriter{}
@@ -294,7 +385,5 @@ func TestStore(t *testing.T) {
 	mdw.callback = sentPacket.findNodeCallback
 	network.dw = mdw
 
-	
+
 }*/
-
-
